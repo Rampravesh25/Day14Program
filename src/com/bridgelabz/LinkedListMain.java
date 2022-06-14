af18f75041c18122dd70d4f5a4be3763cbb698e8
Rampravesh25 <rampraveshmallah@gmail.com>
2022-06-14 23:03:12 +0530

    UC1 Program

---
+++ src/com/bridgelabz/LinkedListMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structure and Algorithm program for Adding the Data ");
+
+        LinkedList list1 = new LinkedList();
+        list1.push(56);
+        list1.push(30);
+        list1.push(70);
+    }
+}
+
---
+++ src/com/bridgelabz/Node.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Node<K> {
+    Node<K> next;
+    K key;
+    Node(K key) {
+        this.key = key;
+    }
+    Node() {
+    }
+}
